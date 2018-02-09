c0aeae49b6b0159322db54fc0a7910d344ef4704
Rui <2221870259@qq.com>
2018-02-09 23:25:43 +0800

    Update

--- include/octoon/octoon.h
+++ include/octoon/octoon.h
@@ -9,19 +9,19 @@
 #	endif
 #endif
 
-#if defined(_BUILD_PLATFORM_WINDOWS) || defined(_BUILD_PLATFORM_LINUX) || defined(_BUILD_PLATFORM_APPLE)
+#if defined(OCTOON_BUILD_PLATFORM_WINDOWS) || defined(OCTOON_BUILD_PLATFORM_LINUX) || defined(OCTOON_BUILD_PLATFORM_APPLE)
 #	if defined(OCTOON_BUILD_DLL_EXPORT)
 #		define OCTOON_EXPORT
 #	else
 #		define OCTOON_EXPORT
 #	endif
 
-#	if defined(_BUILD_PLATFORM_WINDOWS)
+#	if defined(OCTOON_BUILD_PLATFORM_WINDOWS)
 #		define OCTOON_CALL __stdcall
 #	else
 #		define OCTOON_CALL
 #	endif
-#elif defined(_BUILD_PLATFORM_ANDROID)
+#elif defined(OCTOON_BUILD_PLATFORM_ANDROID)
 #	include <jni.h>
 #	ifndef OCTOON_EXPORT
 #		define OCTOON_EXPORT JNIEXPORT
@@ -39,21 +39,21 @@ OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL OctoonCloseWindow() noexcept;
 OCTOON_C_LINKAGE OCTOON_EXPORT bool OCTOON_CALL OctoonIsQuitRequest() noexcept;
 OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL OctoonUpdate() noexcept;
 
-#if defined(_BUILD_PLATFORM_ANDROID)
+#if defined(OCTOON_BUILD_PLATFORM_JAVA)
 
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeConfig(JNIEnv*  env, jobject thiz, jstring gamedir, jstring scenename, jboolean bShader) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeStorage(JNIEnv*  env, jobject thiz, jint type, jstring pakagename, jstring apklocation) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeOpen(JNIEnv*  env, jobject thiz, jint w, jint h, jfloat dpiw, jfloat dpih, jobject assetManager) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeClose(JNIEnv* env, jobject thiz) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeRender(JNIEnv* env, jobject obj) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeOnPause(JNIEnv* env, jobject obj) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeOnResume(JNIEnv* env, jobject obj) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeOnStop(JNIEnv* env, jobject obj) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeTouchesBegin(JNIEnv* env, jobject obj, jint pIDs, jfloat pXs, jfloat pYs) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeTouchesMove(JNIEnv* env, jobject obj, jintArray pIDs, jfloatArray pXs, jfloatArray pYs) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeTouchesEnd(JNIEnv* env, jobject obj, jint pIDs, jfloat pXs, jfloat pYs) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeTouchesCancel(JNIEnv* env, jobject obj, jintArray pIDs, jfloatArray pXs, jfloatArray pYs) noexcept;
-OCTOON_C_LINKAGE OCTOON_EXPORT void RAY_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeKeyPress(JNIEnv* env, jobject obj, jint pKeyCode) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeConfig(JNIEnv*  env, jobject thiz, jstring gamedir, jstring scenename, jboolean bShader) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeStorage(JNIEnv*  env, jobject thiz, jint type, jstring pakagename, jstring apklocation) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeOpen(JNIEnv*  env, jobject thiz, jint w, jint h, jfloat dpiw, jfloat dpih, jobject assetManager) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeClose(JNIEnv* env, jobject thiz) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeRender(JNIEnv* env, jobject obj) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeOnPause(JNIEnv* env, jobject obj) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeOnResume(JNIEnv* env, jobject obj) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeOnStop(JNIEnv* env, jobject obj) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeTouchesBegin(JNIEnv* env, jobject obj, jint pIDs, jfloat pXs, jfloat pYs) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeTouchesMove(JNIEnv* env, jobject obj, jintArray pIDs, jfloatArray pXs, jfloatArray pYs) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeTouchesEnd(JNIEnv* env, jobject obj, jint pIDs, jfloat pXs, jfloat pYs) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeTouchesCancel(JNIEnv* env, jobject obj, jintArray pIDs, jfloatArray pXs, jfloatArray pYs) noexcept;
+OCTOON_C_LINKAGE OCTOON_EXPORT void OCTOON_CALL Java_org_octoon_lib_Octoon3DRenderer_nativeKeyPress(JNIEnv* env, jobject obj, jint pKeyCode) noexcept;
 
 #endif
 

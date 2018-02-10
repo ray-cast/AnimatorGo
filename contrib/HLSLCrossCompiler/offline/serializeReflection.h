efcf401c6c30b5ee38fdfbd0d899ac428080c650
Rui <2221870259@qq.com>
2018-02-10 14:36:47 +0800

    Add 3party

---
+++ contrib/HLSLCrossCompiler/offline/serializeReflection.h
@@ -0,0 +1,8 @@
+#ifndef SERIALIZE_REFLECTION_H_
+#define SERIALIZE_REFLECTION_H_
+
+#include "hlslcc.h"
+
+const char* SerializeReflection(ShaderInfo* psReflection);
+
+#endif
---
+++ contrib/HLSLCrossCompiler/offline/timer.h
@@ -0,0 +1,26 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+typedef struct
+{
+#ifdef _WIN32
+    LARGE_INTEGER frequency;
+    LARGE_INTEGER startCount;
+    LARGE_INTEGER endCount;
+#else
+    struct timeval startCount;
+    struct timeval endCount;
+#endif
+} Timer_t;
+
+void InitTimer(Timer_t* psTimer);
+void ResetTimer(Timer_t* psTimer);
+double ReadTimer(Timer_t* psTimer);
+
+#endif

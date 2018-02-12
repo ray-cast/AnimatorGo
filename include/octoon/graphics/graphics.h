39575b51cc62044fb94fbfe7d37983522d4a32d9
Rui <2221870259@qq.com>
2018-02-12 21:44:05 +0800

    Add graphics.h

---
+++ include/octoon/graphics/graphics.h
@@ -0,0 +1,16 @@
+#ifndef OCTOON_GRAPHICS_H_
+#define OCTOON_GRAPHICS_H_
+
+#include <octoon/graphics/graphics_system.h>
+#include <octoon/graphics/graphics_device.h>
+#include <octoon/graphics/graphics_context.h>
+#include <octoon/graphics/graphics_swapchain.h>
+#include <octoon/graphics/graphics_data.h>
+#include <octoon/graphics/graphics_texture.h>
+#include <octoon/graphics/graphics_state.h>
+#include <octoon/graphics/graphics_shader.h>
+#include <octoon/graphics/graphics_pipeline.h>
+#include <octoon/graphics/graphics_input_layout.h>
+#include <octoon/graphics/graphics_descriptor.h>
+
+#endif

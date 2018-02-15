df580b407290898383817c4063f3381c1f4c39ce
JunWang <jstzwj@aliyun.com>
2018-02-15 19:39:17 +0800

    add some source files

--- include/octoon/audio/common/audio_device.h
+++ include/octoon/audio/common/audio_device.h
@@ -1,7 +1,13 @@
 #ifndef OCTOON_AUDIO_DEVICE_H_
 #define OCTOON_AUDIO_DEVICE_H_
 
+#include <memory>
+
 #include <octoon/runtime/platform.h>
+
+#include <octoon/audio/common/audio_source.h>
+#include <octoon/audio/common/audio_listener.h>
+
 
 
 namespace octoon
@@ -22,8 +28,8 @@ namespace octoon
             virtual void setDistanceModel(bool enable) noexcept = 0;
             virtual bool getDistanceModel() const noexcept = 0;
 
-            virtual SoundSourcePtr createSoundSource() = 0;
-            virtual SoundListenerPtr createSoundListener() noexcept = 0;
+            virtual std::shared_ptr<AudioSource> createSoundSource() = 0;
+            virtual std::shared_ptr<AudioListener> createSoundListener() noexcept = 0;
         };
     }
 }
---
+++ source/octoon-audio/common/audio_listener.cpp
@@ -0,0 +1,15 @@
+#include <octoon/audio/common/audio_listener.h>
+
+namespace octoon
+{
+    namespace audio
+    {
+        AudioListener::AudioListener() noexcept
+        {
+        }
+
+        AudioListener::~AudioListener() noexcept
+        {
+        }
+    }
+}

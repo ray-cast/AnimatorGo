98665d0884c22e3a1940f27f6c33a4e44aaac142
Rui <2221870259@qq.com>
2018-02-21 00:42:09 +0800

    Rename

--- include/octoon/transform_component.h
+++ include/octoon/transform_component.h
@@ -13,11 +13,10 @@ namespace octoon
 		~TransformComponent();
 
 		void set_translate(const math::float3& v) noexcept;
-		void set_translateAccum(const math::float3& v) noexcept;
+		void set_translate_accum(const math::float3& v) noexcept;
 		const math::float3& get_translate() const noexcept;
 
 		void set_scale(const math::float3& v) noexcept;
-		void set_scale_all(const float v) noexcept;
 		void set_scale_accum(const math::float3& v) noexcept;
 		const math::float3& get_scale() const noexcept;
 
@@ -30,26 +29,27 @@ namespace octoon
 		const math::float4x4& get_transform() const noexcept;
 		const math::float4x4& get_transform_inverse() const noexcept;
 
-		void set_world_translate(const math::float3& v) noexcept;
-		void set_world_translate_accum(const math::float3& v) noexcept;
-		const math::float3& get_world_translate() const noexcept;
+		void set_local_translate(const math::float3& v) noexcept;
+		void set_local_translate_accum(const math::float3& v) noexcept;
+		const math::float3& get_local_translate() const noexcept;
 
-		void set_world_scale(const math::float3& v) noexcept;
-		void set_world_scaleAccum(const math::float3& v) noexcept;
-		const math::float3& get_world_scale() const noexcept;
+		void set_local_scale(const math::float3& v) noexcept;
+		void set_local_scale_all(const float v) noexcept;
+		void set_local_scale_accum(const math::float3& v) noexcept;
+		const math::float3& get_local_scale() const noexcept;
 
-		void set_world_quaternion(const math::Quaternion& quat) noexcept;
-		void set_world_quaternion_accum(const math::Quaternion& quat) noexcept;
-		const math::Quaternion& get_world_quaternion() const noexcept;
+		void set_local_quaternion(const math::Quaternion& quat) noexcept;
+		void set_local_quaternion_accum(const math::Quaternion& quat) noexcept;
+		const math::Quaternion& get_local_quaternion() const noexcept;
+
+		void set_local_transform(const math::float4x4& transform) noexcept;
+		void set_local_transform_only_rotate(const math::float4x4& transform) noexcept;
+		const math::float4x4& get_local_transform() const noexcept;
+		const math::float4x4& get_local_transform_inverse() const noexcept;
 
 		const math::float3& get_right() const noexcept;
 		const math::float3& get_up() const noexcept;
 		const math::float3& get_forward() const noexcept;
-
-		void set_world_transform(const math::float4x4& transform) noexcept;
-		void set_world_transform_only_rotate(const math::float4x4& transform) noexcept;
-		const math::float4x4& get_world_transform() const noexcept;
-		const math::float4x4& get_world_transform_inverse() const noexcept;
 
 		virtual GameComponentPtr clone() const noexcept;
 
@@ -65,20 +65,19 @@ namespace octoon
 		void update_parent_transform() const noexcept;
 
 	private:
+		mutable math::float3 translate_;
+		mutable math::float3 scaling_;
+		mutable math::Quaternion rotation_;
 
 		mutable math::float3 local_translate_;
 		mutable math::float3 local_scaling_;
 		mutable math::Quaternion local_rotation_;
 
-		mutable math::float3 world_translate_;
-		mutable math::float3 world_scaling_;
-		mutable math::Quaternion world_rotation_;
+		mutable math::float4x4 transform_;
+		mutable math::float4x4 transform_inverse_;
 
 		mutable math::float4x4 local_transform_;
 		mutable math::float4x4 local_transform_inverse_;
-
-		mutable math::float4x4 world_transform_;
-		mutable math::float4x4 world_transform_inverse_;
 
 		mutable bool local_need_updates_;
 		mutable bool world_need_updates_;

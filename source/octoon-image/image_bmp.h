62b4615f34f4f6819c7adc50d1c6e6b7c6b57a0e
Rui <2221870259@qq.com>
2018-02-21 18:39:38 +0800

    add override

--- source/octoon-image/image_bmp.h
+++ source/octoon-image/image_bmp.h
@@ -15,11 +15,11 @@ namespace octoon
 			BMPHandler() noexcept = default;
 			virtual ~BMPHandler() = default;
 
-			bool do_can_read(istream& stream) const noexcept;
-			bool do_can_read(const char* type_name) const noexcept;
+			bool do_can_read(istream& stream) const noexcept override;
+			bool do_can_read(const char* type_name) const noexcept override;
 
-			bool do_load(istream& stream, Image& image) except;
-			bool do_save(ostream& stream, const Image& image) except;
+			bool do_load(istream& stream, Image& image) except override;
+			bool do_save(ostream& stream, const Image& image) except override;
 
 		private:
 			bool decode(istream& stream, Image& image, const BITMAPINFO& info);
--- source/octoon-image/image_dds.h
+++ source/octoon-image/image_dds.h
@@ -13,11 +13,11 @@ namespace octoon
 			DDSHandler() noexcept = default;
 			virtual ~DDSHandler() = default;
 
-			bool do_can_read(istream& stream) const noexcept;
-			bool do_can_read(const char* type_name) const noexcept;
+			bool do_can_read(istream& stream) const noexcept override;
+			bool do_can_read(const char* type_name) const noexcept override;
 
-			bool do_load(istream& stream, Image& image) noexcept;
-			bool do_save(ostream& stream, const Image& image) noexcept;
+			bool do_load(istream& stream, Image& image) noexcept override;
+			bool do_save(ostream& stream, const Image& image) noexcept override;
 
 		private:
 			DDSHandler(const DDSHandler&) noexcept = delete;

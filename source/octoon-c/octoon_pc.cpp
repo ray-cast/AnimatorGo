4065ba7310339c79311b7cb3fc3b16ec2ec2cb0e
Rui <2221870259@qq.com>
2018-02-22 22:48:38 +0800

    cleanup space and tab

--- source/octoon-c/octoon_pc.cpp
+++ source/octoon-c/octoon_pc.cpp
@@ -370,58 +370,58 @@ void onWindowDrop(GLFWwindow* window, int count, const char** file_utf8)
 }
 
 #ifndef GLFW_EXPOSE_NATIVE_WIN32
-void _split_whole_name(const char *whole_name, char *fname, char *ext)  
-{  
-	const char *p_ext;  
-  
-	p_ext = rindex(whole_name, '.');  
-	if (NULL != p_ext)  
-	{  
-		strcpy(ext, p_ext);  
-		snprintf(fname, p_ext - whole_name + 1, "%s", whole_name);  
-	}  
-	else  
-	{  
-		ext[0] = '\0';  
-		strcpy(fname, whole_name);  
-	}  
-}  
+void _split_whole_name(const char *whole_name, char *fname, char *ext)
+{
+	const char *p_ext;
 
-void _splitpath(const char *path, char *drive, char *dir, char *fname, char *ext)  
-{  
-	const char *p_whole_name;  
-  
-	drive[0] = '\0';  
-	if (NULL == path)  
-	{  
-		dir[0] = '\0';  
-		fname[0] = '\0';  
-		ext[0] = '\0';  
-		return;  
-	}  
-  
-	if ('/' == path[strlen(path)])  
-	{  
-		strcpy(dir, path);  
-		fname[0] = '\0';  
-		ext[0] = '\0';  
-		return;  
-	}  
-  
-	p_whole_name = rindex(path, '/');  
-	if (NULL != p_whole_name)  
-	{  
-		p_whole_name++;  
-		_split_whole_name(p_whole_name, fname, ext);  
-  
-		snprintf(dir, p_whole_name - path, "%s", path);  
-	}  
-	else  
-	{  
-		_split_whole_name(path, fname, ext);  
-		dir[0] = '\0';  
-	}  
-}  
+	p_ext = rindex(whole_name, '.');
+	if (NULL != p_ext)
+	{
+		strcpy(ext, p_ext);
+		snprintf(fname, p_ext - whole_name + 1, "%s", whole_name);
+	}
+	else
+	{
+		ext[0] = '\0';
+		strcpy(fname, whole_name);
+	}
+}
+
+void _splitpath(const char *path, char *drive, char *dir, char *fname, char *ext)
+{
+	const char *p_whole_name;
+
+	drive[0] = '\0';
+	if (NULL == path)
+	{
+		dir[0] = '\0';
+		fname[0] = '\0';
+		ext[0] = '\0';
+		return;
+	}
+
+	if ('/' == path[strlen(path)])
+	{
+		strcpy(dir, path);
+		fname[0] = '\0';
+		ext[0] = '\0';
+		return;
+	}
+
+	p_whole_name = rindex(path, '/');
+	if (NULL != p_whole_name)
+	{
+		p_whole_name++;
+		_split_whole_name(p_whole_name, fname, ext);
+
+		snprintf(dir, p_whole_name - path, "%s", path);
+	}
+	else
+	{
+		_split_whole_name(path, fname, ext);
+		dir[0] = '\0';
+	}
+}
 #endif
 
 bool OCTOON_CALL OctoonInit(const char* gamedir, const char* scenename) noexcept

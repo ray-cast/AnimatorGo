16ed64a0376e4e529f126801da957b0234caffe6
Rui <2221870259@qq.com>
2018-02-23 14:42:30 +0800

    Code Refactorings

--- include/octoon/input/input.h
+++ include/octoon/input/input.h
@@ -7,69 +7,69 @@ namespace octoon
 {
 	namespace input
 	{
-		class OCTOON_EXPORT DefaultInput final : public IInput
+		class OCTOON_EXPORT DefaultInput : public IInput
 		{
 			OctoonDeclareSubClass(DefaultInput, IInput)
 		public:
 			DefaultInput() noexcept;
-			~DefaultInput() noexcept;
+			virtual ~DefaultInput() noexcept;
 
-			virtual bool open() noexcept;
-			virtual bool open(InputDevicePtr& device) noexcept;
-			virtual bool open(InputDevicePtr&& device) noexcept;
-			virtual void close() noexcept;
+			virtual bool open() noexcept override;
+			virtual bool open(InputDevicePtr& device) noexcept override;
+			virtual bool open(InputDevicePtr&& device) noexcept override;
+			virtual void close() noexcept override;
 
-			virtual void set_capture_object(WindHandle window) noexcept;
-			virtual WindHandle get_capture_object() const noexcept;
+			virtual void set_capture_object(WindHandle window) noexcept override;
+			virtual WindHandle get_capture_object() const noexcept override;
 
-			virtual float get_axis(InputAxis::Code axis) const noexcept;
+			virtual float get_axis(InputAxis::Code axis) const noexcept override;
 
-			virtual void set_mouse_pos(InputButton::mouse_t x, InputButton::mouse_t y) noexcept;
-			virtual void get_mouse_pos(InputButton::mouse_t& x, InputButton::mouse_t& y) const noexcept;
+			virtual void set_mouse_pos(InputButton::Type x, InputButton::Type y) noexcept override;
+			virtual void get_mouse_pos(InputButton::Type& x, InputButton::Type& y) const noexcept override;
 
-			virtual bool is_key_down(InputKey::Code key) const noexcept;
-			virtual bool is_key_up(InputKey::Code key) const noexcept;
-			virtual bool is_key_pressed(InputKey::Code key) const noexcept;
+			virtual bool is_key_down(InputKey::Code key) const noexcept override;
+			virtual bool is_key_up(InputKey::Code key) const noexcept override;
+			virtual bool is_key_pressed(InputKey::Code key) const noexcept override;
 
-			virtual bool is_button_down(InputButton::Code key) const noexcept;
-			virtual bool is_button_up(InputButton::Code key) const noexcept;
-			virtual bool is_button_pressed(InputButton::Code key) const noexcept;
+			virtual bool is_button_down(InputButton::Code key) const noexcept override;
+			virtual bool is_button_up(InputButton::Code key) const noexcept override;
+			virtual bool is_button_pressed(InputButton::Code key) const noexcept override;
 
-			virtual void show_cursor(bool show) noexcept;
-			virtual bool is_show_cursor() const noexcept;
+			virtual void show_cursor(bool show) noexcept override;
+			virtual bool is_show_cursor() const noexcept override;
 
-			virtual void lock_cursor(bool lock) noexcept;
-			virtual bool is_locked_cursor() const noexcept;
+			virtual void lock_cursor(bool lock) noexcept override;
+			virtual bool is_locked_cursor() const noexcept override;
 
-			virtual void obtain_mouse_capture() noexcept;
-			virtual void obtain_keyboard_capture() noexcept;
+			virtual void obtain_mouse_capture() noexcept override;
+			virtual void obtain_keyboard_capture() noexcept override;
 
-			virtual void obtain_mouse_capture(InputMousePtr& mouse) noexcept;
-			virtual void obtain_mouse_capture(InputMousePtr&& mouse) noexcept;
-			virtual void obtain_keyboard_capture(InputKeyboardPtr& key) noexcept;
-			virtual void obtain_keyboard_capture(InputKeyboardPtr&& key) noexcept;
-			virtual void obtain_capture() noexcept;
+			virtual void obtain_mouse_capture(InputMousePtr& mouse) noexcept override;
+			virtual void obtain_mouse_capture(InputMousePtr&& mouse) noexcept override;
+			virtual void obtain_keyboard_capture(InputKeyboardPtr& key) noexcept override;
+			virtual void obtain_keyboard_capture(InputKeyboardPtr&& key) noexcept override;
+			virtual void obtain_capture() noexcept override;
 
-			virtual void release_mouse_capture() noexcept;
-			virtual void release_keyboard_capture() noexcept;
-			virtual void release_capture() noexcept;
+			virtual void release_mouse_capture() noexcept override;
+			virtual void release_keyboard_capture() noexcept override;
+			virtual void release_capture() noexcept override;
 
-			virtual void reset() noexcept;
+			virtual void reset() noexcept override;
 
-			virtual void add_input_listener(InputListenerPtr& listener) noexcept;
-			virtual void add_input_listener(InputListenerPtr&& listener) noexcept;
-			virtual void remove_input_listener(InputListenerPtr& listener) noexcept;
-			virtual void remove_input_listener(InputListenerPtr&& listener) noexcept;
-			virtual void clear_input_listener() noexcept;
+			virtual void add_input_listener(InputListenerPtr& listener) noexcept override;
+			virtual void add_input_listener(InputListenerPtr&& listener) noexcept override;
+			virtual void remove_input_listener(InputListenerPtr& listener) noexcept override;
+			virtual void remove_input_listener(InputListenerPtr&& listener) noexcept override;
+			virtual void clear_input_listener() noexcept override;
 
-			virtual bool send_input_event(const InputEvent& event) noexcept;
-			virtual bool post_input_event(const InputEvent& event) noexcept;
+			virtual bool send_input_event(const InputEvent& event) noexcept override;
+			virtual bool post_input_event(const InputEvent& event) noexcept override;
 
-			virtual void update_begin() noexcept;
-			virtual void update() noexcept;
-			virtual void update_end() noexcept;
+			virtual void update_begin() noexcept override;
+			virtual void update() noexcept override;
+			virtual void update_end() noexcept override;
 
-			virtual InputPtr clone() const noexcept;
+			virtual InputPtr clone() const noexcept override;
 
 		private:
 			DefaultInput(const DefaultInput&) noexcept = delete;
--- source/octoon-input/glfw_input_keyboard.cpp
+++ source/octoon-input/glfw_input_keyboard.cpp
@@ -147,11 +147,27 @@ namespace octoon
 		}
 
 		bool
-		GLFWInputKeyboard::get_key_state(InputKey::Code key) const noexcept
+		GLFWInputKeyboard::is_key_down(InputKey::Code key) const noexcept
 		{
 			if (!window_)
 				return false;
-			return ::glfwGetKey((GLFWwindow*)window_, InputKeyToVirtualKey(key)) == GLFW_PRESS ? true : false;;
+			return ::glfwGetKey((GLFWwindow*)window_, InputKeyToVirtualKey(key)) == GLFW_PRESS ? true : false;
+		}
+
+		bool
+		GLFWInputKeyboard::is_key_up(InputKey::Code key) const noexcept
+		{
+			if (!window_)
+				return false;
+			return ::glfwGetKey((GLFWwindow*)window_, InputKeyToVirtualKey(key)) == GLFW_PRESS ? false : true;
+		}
+
+		bool
+		GLFWInputKeyboard::is_key_pressed(InputKey::Code key) const noexcept
+		{
+			if (!window_)
+				return false;
+			return ::glfwGetKey((GLFWwindow*)window_, InputKeyToVirtualKey(key)) == GLFW_REPEAT ? true : false;
 		}
 
 		void

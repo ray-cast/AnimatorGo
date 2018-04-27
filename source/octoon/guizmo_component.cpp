830e55c941af7d48cc73ae44ae89e794b46d898d
Rui <2221870259@qq.com>
2018-04-27 11:10:54 +0800

    update

--- source/octoon/guizmo_component.cpp
+++ source/octoon/guizmo_component.cpp
@@ -32,25 +32,28 @@ namespace octoon
 	void
 	GuizmoComponent::onGui() noexcept
 	{
-		auto transform = this->getComponent<TransformComponent>();
+		if (camera_)
+		{
+			auto transform = this->getComponent<TransformComponent>();
 
-		auto& view = camera_->getComponent<CameraComponent>()->getView();
-		auto& project = camera_->getComponent<CameraComponent>()->getProjection();
-		auto model = transform->getLocalTransform();
+			auto& view = camera_->getComponent<CameraComponent>()->getView();
+			auto& project = camera_->getComponent<CameraComponent>()->getProjection();
+			auto model = transform->getLocalTransform();
 
-		static imgui::guizmo::Operation op(imgui::guizmo::Rotation);
-		if (imgui::is_key_pressed(input::InputKey::Code::Q))
-			op = imgui::guizmo::Translate;
-		if (imgui::is_key_pressed(input::InputKey::Code::W))
-			op = imgui::guizmo::Rotation;
-		if (imgui::is_key_pressed(input::InputKey::Code::E))
-			op = imgui::guizmo::Scale;
+			static imgui::guizmo::Operation op(imgui::guizmo::Rotation);
+			if (imgui::is_key_pressed(input::InputKey::Code::Q))
+				op = imgui::guizmo::Translate;
+			if (imgui::is_key_pressed(input::InputKey::Code::W))
+				op = imgui::guizmo::Rotation;
+			if (imgui::is_key_pressed(input::InputKey::Code::E))
+				op = imgui::guizmo::Scale;
 
-		imgui::guizmo::BeginFrame();
-		imgui::guizmo::SetRect(0, 0, imgui::get_display_size().x, imgui::get_display_size().y);
-		imgui::guizmo::Manipulate(view.ptr(), project.ptr(), op, imgui::guizmo::Mode::Local, model.ptr());
+			imgui::guizmo::BeginFrame();
+			imgui::guizmo::SetRect(0, 0, imgui::get_display_size().x, imgui::get_display_size().y);
+			imgui::guizmo::Manipulate(view.ptr(), project.ptr(), op, imgui::guizmo::Mode::Local, model.ptr());
 
-		transform->setLocalTransform(model);
+			transform->setLocalTransform(model);
+		}
 	}
 
 	GameComponentPtr

101521cfccfb7fc2698c608b4fc288f5659582cc
Rui <2221870259@qq.com>
2018-05-12 21:24:10 +0800

    compiler error

--- source/octoon-graphics/OpenGL Common/ogl_basic.h
+++ source/octoon-graphics/OpenGL Common/ogl_basic.h
@@ -30,7 +30,7 @@
 #	include <OpenGL/CGLTypes.h>
 #elif defined(OCTOON_BUILD_PLATFORM_EMSCRIPTEN) || defined(OCTOON_BUILD_PLATFORM_ANDROID) || defined(OCTOON_BUILD_PLATFORM_JAVA)
 #	include <EGL/egl.h>
-#	include <GLES3/gl3.h>
+#	include <GLES3/gl32.h>
 #endif
 
 #if OCTOON_BUILD_PLATFORM_LINUX

c9dfd6c4560331a9efc5d2490a1e45e5cdb71889
Rui <2221870259@qq.com>
2018-07-13 01:25:28 +0800

    impl of serializable

---
+++ include/octoon/io/archive.h
@@ -0,0 +1,107 @@
+#ifndef OCTOON_ARCHIVE_H_
+#define OCTOON_ARCHIVE_H_
+
+#include <octoon/io/iosbase.h>
+#include <octoon/io/archive_buf.h>
+
+namespace octoon
+{
+	namespace io
+	{
+		class OCTOON_EXPORT archive : public ios_base
+		{
+		public:
+			using type_t = archivebuf::type_t;
+			using boolean_t = archivebuf::boolean_t;
+			using number_integer_t = archivebuf::number_integer_t;
+			using number_unsigned_t = archivebuf::number_unsigned_t;
+			using number_float_t = archivebuf::number_float_t;
+			using number_float2_t = archivebuf::number_float2_t;
+			using number_float3_t = archivebuf::number_float3_t;
+			using number_float4_t = archivebuf::number_float4_t;
+			using number_quaternion_t = archivebuf::number_quaternion_t;
+			using string_t = archivebuf::string_t;
+			using object_t = archivebuf::object_t;
+			using array_t = archivebuf::array_t;
+			using iterator = archivebuf::iterator;
+			using reverse_iterator = archivebuf::reverse_iterator;
+			using const_iterator = archivebuf::const_iterator;
+			using const_reverse_iterator = archivebuf::const_reverse_iterator;
+
+		public:
+			archive() noexcept;
+			virtual ~archive() noexcept;
+
+			void setOpenMode(ios_base::openmode mode) noexcept;
+			ios_base::openmode getOpenMode() const noexcept;
+
+			archivebuf* rdbuf() const noexcept;
+			void set_rdbuf(archivebuf* buf) noexcept;
+
+			std::size_t size() const noexcept;
+
+			const char* type_name() const noexcept;
+			const char* type_name(type_t type) const noexcept;
+
+			const type_t type() const noexcept;
+
+			bool is_null() const noexcept;
+			bool is_boolean() const noexcept;
+			bool is_integral() const noexcept;
+			bool is_float() const noexcept;
+			bool is_string() const noexcept;
+			bool is_numeric() const noexcept;
+			bool is_array() const noexcept;
+			bool is_object() const noexcept;
+
+			template<type_t type, typename = std::enable_if_t<type == type_t::boolean>>
+			constexpr boolean_t get() const except { return this->rdbuf()->get<type>(); }
+
+			template<type_t type, typename = std::enable_if_t<type == type_t::number_integer>>
+			constexpr number_integer_t get() const except { return this->rdbuf()->get<type>(); }
+
+			template<type_t type, typename = std::enable_if_t<type == type_t::number_unsigned>>
+			constexpr number_unsigned_t get() const except { return this->rdbuf()->get<type>(); }
+
+			template<type_t type, typename = std::enable_if_t<type == type_t::number_float>>
+			constexpr number_float_t get() const except { return this->rdbuf()->get<number_float_t, type>(); }
+
+			template<type_t type, typename = std::enable_if_t<type == type_t::string>>
+			constexpr string_t& get() const except { return *this->rdbuf()->get<type>(); }
+
+			template<type_t type, typename = std::enable_if_t<type == type_t::array>>
+			constexpr array_t& get() const except { return *this->rdbuf()->get<type>(); }
+
+			template<typename T, typename = std::enable_if_t<std::is_same<T, boolean_t>::value>>
+			constexpr boolean_t get() const except { return this->rdbuf()->get<type_t::boolean>(); }
+
+			template<typename T, typename = std::enable_if_t<std::is_same<T, number_integer_t>::value>>
+			constexpr number_integer_t get() const except { return this->rdbuf()->get<type_t::number_integer>(); }
+
+			template<typename T, typename = std::enable_if_t<std::is_same<T, number_unsigned_t>::value>>
+			constexpr number_unsigned_t get() const except { return this->rdbuf()->get<type_t::number_unsigned>(); }
+
+			template<typename T, typename = std::enable_if_t<std::is_same<T, number_float_t>::value>>
+			constexpr number_float_t get() const except { return this->rdbuf()->get<type_t::number_float>(); }
+
+			template<typename T, typename = std::enable_if_t<std::is_same<T, string_t>::value>>
+			constexpr string_t& get() const except { return this->rdbuf()->get<type_t::string>(); }
+
+			template<typename T, typename = std::enable_if_t<std::is_same<T, array_t>::value>>
+			constexpr array_t& get() const except { return this->rdbuf()->get<type_t::array>(); }
+
+		protected:
+			void _init(archivebuf* _buf, ios_base::openmode mode) noexcept;
+
+		private:
+			archive & operator=(const archive&) = delete;
+			archive(const archive&) = delete;
+
+		private:
+			archivebuf * _strbuf;
+			ios_base::openmode _mode;
+		};
+	}
+}
+
+#endif
---
+++ source/octoon-io/archive.cpp
@@ -0,0 +1,119 @@
+#include <octoon/io/archive.h>
+
+namespace octoon
+{
+	namespace io
+	{
+		archive::archive() noexcept
+		{
+		}
+
+		archive::~archive() noexcept
+		{
+		}
+
+		archivebuf*
+		archive::rdbuf() const noexcept
+		{
+			return _strbuf;
+		}
+
+		void
+		archive::set_rdbuf(archivebuf* buf) noexcept
+		{
+			_strbuf = buf;
+		}
+
+		void
+		archive::_init(archivebuf* _buf, ios_base::openmode mode) noexcept
+		{
+			this->set_rdbuf(_buf);
+			_mode = mode;
+			ios_base::_init(mode);
+		}
+
+		void
+		archive::setOpenMode(ios_base::openmode mode) noexcept
+		{
+			_mode = mode;
+		}
+
+		ios_base::openmode
+		archive::getOpenMode() const noexcept
+		{
+			return _mode;
+		}
+
+		std::size_t
+		archive::size() const noexcept
+		{
+			return this->rdbuf()->size();
+		}
+
+		const char*
+		archive::type_name() const noexcept
+		{
+			return this->rdbuf()->type_name();
+		}
+
+		const char*
+		archive::type_name(type_t type) const noexcept
+		{
+			return this->rdbuf()->type_name(type);
+		}
+
+		const archive::type_t
+		archive::type() const noexcept
+		{
+			return this->rdbuf()->type();
+		}
+
+		bool
+		archive::is_null() const noexcept
+		{
+			return this->rdbuf()->is_null();
+		}
+
+		bool
+		archive::is_boolean() const noexcept
+		{
+			return this->rdbuf()->is_boolean();
+		}
+
+		bool
+		archive::is_integral() const noexcept
+		{
+			return this->rdbuf()->is_integral();
+		}
+
+		bool
+		archive::is_float() const noexcept
+		{
+			return this->rdbuf()->is_float();
+		}
+
+		bool
+		archive::is_string() const noexcept
+		{
+			return this->rdbuf()->is_string();
+		}
+
+		bool
+		archive::is_numeric() const noexcept
+		{
+			return this->rdbuf()->is_numeric();
+		}
+
+		bool
+		archive::is_array() const noexcept
+		{
+			return this->rdbuf()->is_array();
+		}
+
+		bool
+		archive::is_object() const noexcept
+		{
+			return this->rdbuf()->is_object();
+		}
+	}
+}

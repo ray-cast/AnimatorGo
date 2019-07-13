bbd5f609d90d0447fe8f8611e3244d863bef7168
ray-cast <2221870259@qq.com>
2019-07-13 10:26:42 +0800

    Add PhysX

--- install/build_scripts/physx_config.bat
+++
@@ -1,9 +0,0 @@
-cd %1
-SET PHYSX_ROOT_DIR=%CD%
-SET PHYSX_ROOT_DIR=%PHYSX_ROOT_DIR:\=/%
-SET PM_VSWHERE_PATH=%PHYSX_ROOT_DIR%/../externals/VsWhere
-SET PM_CMAKEMODULES_PATH=%PHYSX_ROOT_DIR%/../externals/CMakeModules
-SET PM_PXSHARED_PATH=%PHYSX_ROOT_DIR%/../pxshared
-SET PM_TARGA_PATH=%PHYSX_ROOT_DIR%/../externals/targa
-SET PM_PATHS=%PM_CMAKEMODULES_PATH%;%PM_TARGA_PATH%
-python ./buildtools/cmake_generate_projects.py %2

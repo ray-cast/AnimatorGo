bb8549b235417db589d7762f05f0500d860fd2b3
ray-cast <2221870259@qq.com>
2019-10-12 01:21:19 +0800

    update

--- source/octoon/solver_component.cpp
+++ source/octoon/solver_component.cpp
@@ -228,15 +228,13 @@ namespace octoon
 						auto& low = limit->getMinimumAxis();
 						auto& upper = limit->getMaximumAxis();
 
-						auto rotation = math::Quaternion::Zero;
+						auto rotation = spin;
 						if ((low.x != 0 || upper.x != 0) && low.y == 0 && upper.y == 0 && low.z == 0 && upper.z == 0)
 							rotation = math::Quaternion(math::float3::UnitX, math::clamp(math::sign(spinAxis.x) * spinAngle, low.x, upper.x));
 						else if ((low.y != 0 || upper.y != 0) && low.x == 0 && upper.x == 0 && low.z == 0 && upper.z == 0)
 							rotation = math::Quaternion(math::float3::UnitY, math::clamp(math::sign(spinAxis.y) * spinAngle, low.x, upper.x));
 						else if ((low.z != 0 || upper.z != 0) && low.x == 0 && upper.x == 0 && low.y == 0 && upper.y == 0)
 							rotation = math::Quaternion(math::float3::UnitZ, math::clamp(math::sign(spinAxis.z) * spinAngle, low.x, upper.x));
-						else
-							rotation = math::Quaternion(spin);
 						
 						transform->setLocalQuaternion(math::normalize(rotation));
 					}

335c74364a4362fa339672bd3e9adcff4fdc5c5a
ray-cast <2221870259@qq.com>
2020-03-27 23:15:33 +0800

    update

--- include/octoon/video/render_types.h
+++ include/octoon/video/render_types.h
@@ -21,28 +21,6 @@ namespace octoon
 		typedef std::vector<RenderPipelinePtr> RenderPipelines;
 		typedef std::vector<RenderScene*> RenderScenes;
 		typedef std::vector<RenderObject*> RenderObjectRaws;
-
-		enum class GraphicsIndexType
-		{
-			Uint16,
-			Uint32,
-		};
-
-		enum class DrawType
-		{
-			Points,
-			Lines,
-			Triangles
-		};
-
-		enum class TextShaingMode
-		{
-			PureColor,
-		};
-
-		typedef std::uint32_t TextColors;
-
-		typedef void* WindHandle;
 	}
 }
 

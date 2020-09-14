e09fbf3301f2e1b12aa62269684d3472038a0bd9
ray-cast <2221870259@qq.com>
2020-09-14 18:22:55 +0800

    update

---
+++ samples/rabbit/libs/oidn-1.2.3/include/OpenImageDenoise/version.h
@@ -0,0 +1,10 @@
+// Copyright 2009-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#define OIDN_VERSION_MAJOR 1
+#define OIDN_VERSION_MINOR 2
+#define OIDN_VERSION_PATCH 3
+#define OIDN_VERSION 10203
+#define OIDN_VERSION_STRING "1.2.3"

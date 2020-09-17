ac4c0809724bb590dc97a550f4ded0b1e7f1f7e8
ray-cast <2221870259@qq.com>
2020-09-17 09:44:00 +0800

    bugfix

--- samples/rabbit/main.cpp
+++ samples/rabbit/main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 
 		auto splash = std::make_unique<rabbit::SplashScreen>();
 		splash->show();
+		app.processEvents();
 
 		rabbit::MainWindow w(splash.get());
 		w.show();

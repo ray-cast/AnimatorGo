739ef684e0676215dbdbc79d251a30bb85c8d703
ray-cast <2221870259@qq.com>
2021-04-04 13:14:52 +0800

    Update

--- source/octoon-core/video/clw_texture_output.cpp
+++ source/octoon-core/video/clw_texture_output.cpp
@@ -1,17 +1,5 @@
 #include "clw_texture_output.h"
 
-#ifdef __APPLE__
-#include <OpenCL/OpenCL.h>
-#include <OpenGL/OpenGL.h>
-#elif WIN32
-#include <Windows.h>
-#include "GL/glew.h"
-#else
-#include <CL/cl.h>
-#include <GL/glew.h>
-#include <GL/glx.h>
-#endif
-
 namespace octoon
 {
 	ClwTextureOutput::ClwTextureOutput(CLWContext context, std::uint32_t texture, std::uint32_t w, std::uint32_t h)
@@ -35,8 +23,8 @@ namespace octoon
 		copyKernel.SetArg(argc++, image_);
 
 		std::size_t globalsize = this->width() * this->height();
-		context_.Launch1D(0, ((globalsize + 63) / 64) * 64, 64, copyKernel);
 
+		context_.Launch1D(0, ((globalsize + 63) / 64) * 64, 64, copyKernel);
 		context_.ReleaseGLObjects(0, objects);
 		context_.Finish(0);
 	}

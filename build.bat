d512dd69666a19781ff6305e71d93ec23b122889
jstzwj <1103870790@qq.com>
2022-05-06 23:31:41 +0800

    copy dll

--- build.bat
+++ build.bat
@@ -3,7 +3,8 @@ if exist build (
 ) else (
   mkdir build && cd build
 )
-conan install ..
+conan install .. -g visual_studio_multi -s arch=x86_64 -s build_type=Debug
+conan install .. -g visual_studio_multi -s arch=x86_64 -s build_type=Release
 cmake .. -G "Visual Studio 16"
 cmake --build . --config Release
 cd ..
